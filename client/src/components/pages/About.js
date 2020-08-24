70bfacab512004cce362574ac3595b5361dd00dd
Yhomi <kareemyomi91@gmail.com>
2020-08-24 23:57:48 +0100

    clean error

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -5,9 +5,9 @@ const About = (props) => {
   return (
     <div>
       <h1>About This App</h1>
-      <p className="my-1">A FullStack React App fro Keeping Contact</p>
+      <p className="my-1">A FullStack React App for Keeping Contact</p>
       <p className="bg-dark p">
-        <strong>Version: </strong>1.0.0
+        <strong>Version: </strong>4.1.0
       </p>
     </div>
   )
